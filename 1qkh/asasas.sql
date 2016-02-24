9301078ae3e71e46c14831d960eb5ef0c4a5f355
JuliaFeng <lijufeng@creditease.cn>
2016-02-24 16:48:57 +0800

    Added files via upload

---
+++ 1qkh/asasas.sql
@@ -0,0 +1,11 @@
+UPDATE `@#_shoplist` SET
+							`canyurenshu`=`zongrenshu`,
+							`shenyurenshu` = '0',
+							`q_uid` = '25',
+							`q_user` = 'a:5:{s:3:"uid";s:2:"25";s:8:"username";s:0:"";s:5:"email";s:0:"";s:6:"mobile";s:11:"18286799655";s:3:"img";s:16:"photo/member.jpg";}',
+							`q_user_code` = '10000104',
+							`q_content`	= '',
+							`q_counttime` ='',
+							`q_end_time` = '1440508859.136',
+							`q_showtime` = 'Y'
+							 where `id` = '3'
